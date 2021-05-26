ed90c800068b70675676b0104e272576ae647ba4
kanakan12 <69573636+kanakan12@users.noreply.github.com>
2021-05-26 14:55:33 +0900

    Installed morgan

--- src/server.js
+++ src/server.js
@@ -1,33 +1,24 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT  = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-const fstMiddleware = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-}
-
-const privateMiddleware = (req, res, next) => {
-    const url = req.url;
-    if(url === "/protected") {
-        return res.send("<h1>Not Allowed</h1>");
-    }
-    console.log("Allowed");
-    next();
-}
-
-const handleProtected = (req, res) => {
-    return res.send("Private")
-}
-
-app.use(fstMiddleware);
-app.use(privateMiddleware);
-
-app.get("/", (req, res) => {
+const home = (req, res) => {
+    console.log("home");
     return res.send("Home");
-});
-app.get("/protected", handleProtected);
+}
+
+const login = (req, res) => {
+    console.log("login");
+    return res.send("login");
+}
+
+app.use(logger);
+
+app.get("/", home);
+app.get("/login", login);
 
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
