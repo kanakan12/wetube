f360223e7c045b22e2a03ba54dd133eab67ae722
kanakan12 <69573636+kanakan12@users.noreply.github.com>
2021-05-26 13:56:40 +0900

    Test Middleware

--- src/server.js
+++ src/server.js
@@ -4,13 +4,30 @@ const PORT  = 4000;
 
 const app = express();
 
-const gossipMiddleware = (req, res, next) => {
-    console.log("middle");
+const fstMiddleware = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
     next();
 }
 
-app.get("/", gossipMiddleware, (req, res) => {
-    return res.send("test");
+const privateMiddleware = (req, res, next) => {
+    const url = req.url;
+    if(url === "/protected") {
+        return res.send("<h1>Not Allowed</h1>");
+    }
+    console.log("Allowed");
+    next();
+}
+
+const handleProtected = (req, res) => {
+    return res.send("Private")
+}
+
+app.use(fstMiddleware);
+app.use(privateMiddleware);
+
+app.get("/", (req, res) => {
+    return res.send("Home");
 });
+app.get("/protected", handleProtected);
 
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
