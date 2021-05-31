104749cb6ef08c27a4958a8a80100bb0da506f60
kanakan12 <69573636+kanakan12@users.noreply.github.com>
2021-05-31 15:11:20 +0900

    Added partials

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,10 +2,10 @@ export const trending = (req, res) => {
     res.render("home");
 }
 export const see = (req, res) => {
-    return res.send("See");
+    res.render("watch");
 }
 export const edit = (req, res) => {
-    return res.send("Edit");
+    res.render("edit");
 }
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
