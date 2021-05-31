101ca4af5fadb92b3193cbf5d013c0624bd94f15
kanakan12 <69573636+kanakan12@users.noreply.github.com>
2021-05-31 15:53:25 +0900

    Added base.pug & MVP.css

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,11 +1,11 @@
 export const trending = (req, res) => {
-    res.render("home");
+    res.render("home", {pageTitle: "Home"});
 }
 export const see = (req, res) => {
-    res.render("watch");
+    res.render("watch", {pageTitle: "Watch"});
 }
 export const edit = (req, res) => {
-    res.render("edit");
+    res.render("edit", {pageTitle: "Edit"});
 }
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
