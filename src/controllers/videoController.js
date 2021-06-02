e9ac79132ea73348d9068b2b04565738cf2cd1ec
kanakan12 <69573636+kanakan12@users.noreply.github.com>
2021-06-02 17:20:54 +0900

    Added watch, edit

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,40 +1,49 @@
+let videos = [
+    {
+        title: "A",
+        rating: 1,
+        comments:0,
+        createdAt: "1 minutes ago",
+        views: 1,
+        id: 1
+    },
+    {
+        title: "B",
+        rating: 2,
+        comments:0,
+        createdAt: "3 minutes ago",
+        views: 10,
+        id: 2
+    },
+    {
+        title: "C",
+        rating: 3,
+        comments:0,
+        createdAt: "5 minutes ago",
+        views: 20,
+        id: 3
+    }
+];
+
 export const trending = (req, res) => {
-    const videos = [
-        {
-            title: "A",
-            rating: 1,
-            comments:0,
-            createdAt: "1 minutes ago",
-            views: 10,
-            id: 1
-        },
-        {
-            title: "B",
-            rating: 2,
-            comments:0,
-            createdAt: "3 minutes ago",
-            views: 10,
-            id: 2
-        },
-        {
-            title: "C",
-            rating: 3,
-            comments:0,
-            createdAt: "5 minutes ago",
-            views: 10,
-            id: 3
-        }
-    ];
     return res.render("home", {pageTitle: "Home", videos});
 }
-export const see = (req, res) => {
-    res.render("watch", {pageTitle: "Watch"});
+
+export const watch = (req, res) => {
+    const { id } = req.params;
+    const video = videos[id - 1];
+    return res.render("watch", {pageTitle: `Watching ${video.title}`, video});
 }
-export const edit = (req, res) => {
-    res.render("edit", {pageTitle: "Edit"});
+
+export const getEdit = (req, res) => {
+    const { id } = req.params;
+    const video = videos[id - 1];
+    return res.render("edit", {pageTitle: `Editing: ${video.title}`, video});
 }
-export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => {
-    return res.send("Delete Video");
-}
+
+export const postEdit = (req, res) => {
+    const { id } = req.params;
+    const { title } = req.body;
+    videos[id - 1].title = title;
+    return res.redirect(`/videos/${id}`);
+};
