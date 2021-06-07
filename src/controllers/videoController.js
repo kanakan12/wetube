89e3bc3f474d8f5532ce6cf56bd46115577a1f0c
kanakan12 <69573636+kanakan12@users.noreply.github.com>
2021-06-07 14:16:10 +0900

    Async Await

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,50 +1,30 @@
-let videos = [
-    {
-        title: "A",
-        rating: 1,
-        comments:0,
-        createdAt: "1 minutes ago",
-        views: 1,
-        id: 1
-    },
-    {
-        title: "B",
-        rating: 2,
-        comments:0,
-        createdAt: "3 minutes ago",
-        views: 10,
-        id: 2
-    },
-    {
-        title: "C",
-        rating: 3,
-        comments:0,
-        createdAt: "5 minutes ago",
-        views: 20,
-        id: 3
-    }
-];
+import { async } from "regenerator-runtime";
+import Video from "../models/Video";
 
-export const trending = (req, res) => {
-    return res.render("home", {pageTitle: "Home", videos});
+export const home = async(req, res) => {
+    try {
+        const videos = await Video.find({
+
+        });
+        return res.render("home", {pageTitle: "Home", videos: []});
+    } catch(error) {
+        return res.render("server-error", {error});
+    }
 }
 
 export const watch = (req, res) => {
     const { id } = req.params;
-    const video = videos[id - 1];
-    return res.render("watch", {pageTitle: `Watching ${video.title}`, video});
+    return res.render("watch", {pageTitle: `Watching`});
 }
 
 export const getEdit = (req, res) => {
     const { id } = req.params;
-    const video = videos[id - 1];
-    return res.render("edit", {pageTitle: `Editing: ${video.title}`, video});
+    return res.render("edit", {pageTitle: `Editing`});
 }
 
 export const postEdit = (req, res) => {
     const { id } = req.params;
     const { title } = req.body;
-    videos[id - 1].title = title;
     return res.redirect(`/videos/${id}`);
 };
 
@@ -54,14 +34,5 @@ export const getUpload = (req, res) => {
 
 export const postUpload = (req, res) => {
     const { title } = req.body;
-    const newVideo = {
-        title,
-        rating: 3,
-        comments:0,
-        createdAt: "5 minutes ago",
-        views: 20,
-        id: videos.length + 1,
-    }
-    videos.push(newVideo);
     return res.redirect("/");
 }
